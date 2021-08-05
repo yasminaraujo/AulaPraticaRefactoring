4dcae54745ababf3e3aae9fc3249bc9ab3ebc12b
Yasmin Araujo <yasmin.araujo@take.net>
2021-08-05 11:27:38 -0300

    Commit 10

--- Movie.java
+++ Movie.java
@@ -42,4 +42,9 @@ public class Movie {
         }
         return thisAmount;
    }
+   public int getFrequentRenterPoints (int daysRented){
+        if ((getPriceCode() == NEW_RELEASE) &&
+         daysRented > 1) return 2;
+        return 1;
+   }
 }

ae1fabd17239268b214fee1ffcf96fea0ac141bc
Yasmin Araujo <yasmin.araujo@take.net>
2021-08-05 11:41:17 -0300

    Commit 14

--- Movie.java
+++ Movie.java
@@ -39,8 +39,6 @@ public class Movie {
        return _price.getCharge(daysRented);
    }
    public int getFrequentRenterPoints (int daysRented){
-        if ((getPriceCode() == NEW_RELEASE) &&
-         daysRented > 1) return 2;
-        return 1;
+         return _price.getFrequentRenterPoints(daysRented);
    }
 }

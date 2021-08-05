f29ff194a6478cdb69686c1ef748b79e132813d2
Yasmin Araujo <yasmin.araujo@take.net>
2021-08-05 11:31:29 -0300

    Commit 11

--- Movie.java
+++ Movie.java
@@ -6,20 +6,32 @@ public class Movie {
 
   private String _title;
   private int _priceCode;
+  private Price _price;
 
-  public Movie(String title, int priceCode) {
-      _title = title;
-      _priceCode = priceCode;
-  }
-
-  public int getPriceCode() {
-      return _priceCode;
-  }
+   public Movie(String name, int priceCode) {
+      _title = name;
+      setPriceCode(priceCode);
+   }
+    
+   public int getPriceCode() {
+      return _price.getPriceCode();
+   }
 
   public void setPriceCode(int arg) {
-      _priceCode = arg;
+      switch (arg) {
+         case REGULAR:
+            _price = new RegularPrice();
+            break;
+         case CHILDRENS:
+            _price = new ChildrensPrice();
+            break;
+         case NEW_RELEASE:
+            _price = new NewReleasePrice();
+            break;
+         default:
+            throw new IllegalArgumentException("Incorrect Price Code");
+      }
   }
-
   public String getTitle (){
       return _title;
   }

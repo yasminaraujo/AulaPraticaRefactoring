c24ad034ad7ff5b43d21a2aaa98758793fd2d0fd
Yasmin Araujo <yasmin.araujo@take.net>
2021-08-05 11:15:55 -0300

    Commit 6

--- Rental.java
+++ Rental.java
@@ -34,4 +34,10 @@ public class Rental {
         }
         return thisAmount;
    }
+   public int getFrequentRenterPoints(){
+        // add bonus for a two day new release rental
+      if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
+         getDaysRented() > 1) return 2;
+    return 1;
+   }
 }

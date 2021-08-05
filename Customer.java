006ea9dfd1de418c5ba3b3d2c1bb0cdbea8c1cd7
Yasmin Araujo <yasmin.araujo@take.net>
2021-08-05 11:11:47 -0300

    Commit 4

--- Customer.java
+++ Customer.java
@@ -26,7 +26,7 @@ public class Customer {
         double thisAmount = 0;
         Rental each = (Rental) rentals.nextElement();
 
-       thisAmount = amountFor(each);
+       thisAmount = each.getCharge();
 
         // add frequent renter points
         frequentRenterPoints ++;
@@ -45,24 +45,5 @@ public class Customer {
      result += "You earned " + String.valueOf(frequentRenterPoints) +
              " frequent renter points";
      return result;
-   }
-   private double amountFor(Rental aRental){
-       double thisAmount = 0;
-        switch (aRental.getMovie().getPriceCode()) {
-           case Movie.REGULAR:
-              thisAmount += 2;
-              if (aRental.getDaysRented() > 2)
-                 thisAmount += (aRental.getDaysRented() - 2) * 1.5;
-              break;
-           case Movie.NEW_RELEASE:
-              thisAmount += aRental.getDaysRented() * 3;
-              break;
-           case Movie.CHILDRENS:
-              thisAmount += 1.5;
-              if (aRental.getDaysRented() > 3)
-                 thisAmount += (aRental.getDaysRented() - 3) * 1.5;
-               break;
-        }
-        return thisAmount;
    }
 }
